16d24357efe0c9a818105129c7402938a5c561f0
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2021-06-27 10:44:18 -0400

    Call SSHClient.close().
    Update code comments with links to paramiko and getpass API docs.

--- ssh_cmd.py
+++ ssh_cmd.py
@@ -1,6 +1,7 @@
 import paramiko
 
 def ssh_command(ip, port, user, passwd, cmd):
+	# See http://docs.paramiko.org/en/stable/api/client.html#paramiko.client.SSHClient.
 	client = paramiko.SSHClient()
 	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	client.connect(ip, port=port, username=user, password=passwd)
@@ -12,9 +13,12 @@ def ssh_command(ip, port, user, passwd, cmd):
 		for line in output:
 			print(line.strip())
 
+	client.close()
+
 if __name__ == '__main__':
 	import getpass
 	user = input('Username: ')
+	# See https://docs.python.org/3/library/getpass.html.
 	password = getpass.getpass()
 
 	ip = input('Enter server IP: ')
