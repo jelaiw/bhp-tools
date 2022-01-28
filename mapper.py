85c50f0a61f8e42f05e92ec2f2ea04ad4cec6e3f
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-01-28 17:03:19 +0000

    Add test_remote() and threads to mapper exercise.

--- mapper.py
+++ mapper.py
@@ -1,10 +1,41 @@
 import queue
 import os
 import contextlib
+import time
+import requests
+import sys
+import threading
 
 FILTERED = [".jpg", ".gif", ".png", ".css"]
+TARGET = "http://example.com"
+THREADS = 8
 
 web_paths = queue.Queue()
+answers = queue.Queue()
+
+def test_remote():
+	while not web_paths.empty():
+		path = web_paths.get()
+		url = f'{TARGET}{path}'
+		time.sleep(2) # Try to avoid target throttling/lockout protection.
+		r = requests.get(url)
+		if r.status_code == 200:
+			answers.put(url)
+			sys.stdout.write('+')
+		else:
+			sys.stdout.write('x')
+		sys.stdout.flush()
+
+def run():
+	threads = list()
+	for i in range(THREADS):
+		print(f'Spawning thread {i}.')
+		t = threading.Thread(target=test_remote)
+		threads.append(t)
+		t.start()
+
+	for t in threads:
+		t.join()
 
 def gather_paths():
 	for root, _, files in os.walk('.'):
@@ -34,3 +65,9 @@ if __name__ == '__main__':
 	with chdir("/home/kali/tmp/wordpress"):
 		gather_paths()
 	input("Press return to continue.")
+
+	run()
+	with open('answers.txt', 'w') as f:
+		while not answers.empty():
+			f.write(f'{answers.get()}\n')
+	print('Done.')
