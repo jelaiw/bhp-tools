6b40ea7038bb1e8fddeb23e55b19e0cf8db00849
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-02-03 18:25:38 +0000

    Add dir_bruter() and main() to bruter exercise.

--- bruter.py
+++ bruter.py
@@ -6,7 +6,7 @@ import sys
 AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:19.0) Gecko/20100101 Firefox/19.0"
 EXTENSIONS = ['.php', '.bak', '.orig', '.inc']
 TARGET = "http://testphp.vulnweb.com"
-THREADS = 29
+THREADS = 5
 WORDLIST = "SVNDigger-all.txt"
 
 def get_words(resume=None):
@@ -37,3 +37,30 @@ def get_words(resume=None):
 			extend_words(word)
 	
 	return words
+
+def dir_bruter(words):
+	headers = {'User-Agent': AGENT}
+	while not words.empty():
+		url = f'{TARGET}{words.get()}'
+		try:
+			r = requests.get(url, headers=headers)
+		except requests.exceptions.ConnectionError:
+			sys.stderr.write('x');sys.stderr.flush()
+			continue
+
+		if r.status_code == 200:
+			print(f'\nSuccess ({r.status_code}: {url})')
+		elif r.status_code == 404:
+			sys.stderr.write('.');sys.stderr.flush()
+		else:
+			print(f'{r.status_code} => {url}')
+
+if __name__ == '__main__':
+	words = get_words()
+	print('Press return to continue.')
+	sys.stdin.readline()
+	for _ in range(THREADS):
+		t = threading.Thread(target=dir_bruter, args=(words,))
+		t.start()
+
+			
